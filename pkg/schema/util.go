044624e27fd39c7fcd97d4a7455693fa8b565315
Jay Pipes <jaypipes@gmail.com>
2020-05-06 09:08:36 -0400

    correct two errors in type definition output
    Two major flaws were present in the type definition and CRD output
    regarding Go type deduction:
    
    * For object types, we were passing in the wrong openapi3.Schema object
      to the getGoTypeFromSchema() method, which was resulting in things
      like this:
    
    ```go
    // TopicStatus defines the observed state of Topic
    type TopicStatus struct {
    	TopicARN *TopicArn `json:"topicARN,omitempty" aws:"TopicArn"`
    }
    ```
    
    instead of this:
    
    ```go
    // TopicStatus defines the observed state of Topic
    type TopicStatus struct {
    	TopicARN string `json:"topicARN,omitempty" aws:"TopicArn"`
    }
    ```
    
    * For array types, we were not propertly deducing the type of the item
      contained in the array if that was an unnamed object type. This
      resulted in the following occurring:
    
    ```go
    // TopicSpec defines the desired state of Topic
    type TopicSpec struct {
    	// The ARN attr is on all AWS service API CRs. It represents the Amazon
    	// CRD Name for the object. CRs of this Kind that are created without
    	// an ARN attr will be created by the controller. CRs of this Kind that
    	// are created with a non-nil ARN attr are considered by the controller to
    	// already exist in the backend AWS service API.
    	ARN *string `json:"arn,omitempty"`
    	Attributes map[string]string `json:"attributes,omitempty" aws:"Attributes"`
    	Name string `json:"name,omitempty" aws:"Name"`
    	Tags []Tags `json:"tags,omitempty" aws:"Tags"`
    }
    ```
    
    instead of this:
    
    ```go
    // TopicSpec defines the desired state of Topic
    type TopicSpec struct {
    	// The ARN attr is on all AWS service API CRs. It represents the Amazon
    	// CRD Name for the object. CRs of this Kind that are created without
    	// an ARN attr will be created by the controller. CRs of this Kind that
    	// are created with a non-nil ARN attr are considered by the controller to
    	// already exist in the backend AWS service API.
    	ARN *string `json:"arn,omitempty"`
    	Attributes map[string]string `json:"attributes,omitempty" aws:"Attributes"`
    	Name string `json:"name,omitempty" aws:"Name"`
    	Tags []*Tag `json:"tags,omitempty" aws:"Tags"`
    }
    ```

--- pkg/schema/util.go
+++ pkg/schema/util.go
@@ -14,9 +14,11 @@
 package schema
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 
+	"github.com/gertd/go-pluralize"
 	"github.com/getkin/kin-openapi/openapi3"
 )
 
@@ -55,22 +57,28 @@ func (h *Helper) getGoTypeFromSchema(
 	case "boolean":
 		return "bool"
 	case "string":
+		if schema.Format == "byte" {
+			return "[]byte"
+		}
 		return "string"
 	case "number":
-		if schema.Format != "" {
-			switch schema.Format {
-			case "int64":
-				return "int64"
-			case "float64":
-				return "float64"
-			}
+		if schema.Format == "float64" {
+			return "float64"
 		}
 		return "int64"
 	case "integer":
 		return "int64"
 	case "array":
 		itemsSchema := h.getSchemaFromSchemaRef(schema.Items)
-		itemType := h.getGoTypeFromSchema(schemaName, itemsSchema)
+		var itemType string
+		if itemsSchema.Type == "object" {
+			itemType = h.deduceArrayOfObjectsType(schemaName, itemsSchema)
+			if itemType == "" {
+				itemType = "!!! UNKNOWN ARRAY OBJECT TYPE !!!"
+			}
+		} else {
+			itemType = h.getGoTypeFromSchema(schemaName, itemsSchema)
+		}
 		return "[]" + itemType
 	case "object":
 		if schema.AdditionalPropertiesAllowed != nil && *schema.AdditionalPropertiesAllowed {
@@ -79,6 +87,73 @@ func (h *Helper) getGoTypeFromSchema(
 		return "*" + schemaName
 	}
 	return "!!! UNKNOWN !!!"
+}
+
+// Some shapes in AWS service APIs are arrays of unnamed objects. A good
+// example of this is the SNS API's TagList shape, transformed into the
+// following JSONSchema:
+//
+//    TagList:
+//      items:
+//        properties:
+//          Key:
+//            $ref: '#/components/schemas/TagKey'
+//          Value:
+//            $ref: '#/components/schemas/TagValue'
+//        required:
+//        - Key
+//        - Value
+//        type: object
+//      type: array
+//    Tag:
+//      properties:
+//        Key:
+//          $ref: '#/components/schemas/TagKey'
+//        Value:
+//          $ref: '#/components/schemas/TagValue'
+//      required:
+//      - Key
+//      - Value
+//      type: object
+//
+// We need to convert a reference to the TagList schema (i.e.
+// #/components/schemas/TagList) into the following Go type definition:
+//
+//   []*Tag
+//
+// Because we don't know that the TagList items are actually Tag objects, we
+// need to try and deduce this information. We first examine the name of the
+// array type and see if there is an object type with the same name without the
+// "List" or "s" or "Set" suffixes. We then check to see if the schema of the
+// found object type is identical to the schema of the items in the array type.
+func (h *Helper) deduceArrayOfObjectsType(
+	arrayTypeName string,
+	objectTypeSchema *openapi3.Schema,
+) string {
+	guessObjectTypeName := ""
+	if strings.HasSuffix(arrayTypeName, "List") {
+		guessObjectTypeName = strings.TrimSuffix(arrayTypeName, "List")
+	} else if strings.HasSuffix(arrayTypeName, "Set") {
+
+	} else {
+		// Fall back to determining if the array type name is a plural
+		pluralize := pluralize.NewClient()
+		if pluralize.IsPlural(arrayTypeName) {
+			guessObjectTypeName = pluralize.Singular(arrayTypeName)
+		}
+	}
+	if guessObjectTypeName != "" {
+		// Check to see if there is a schema matching the guessed object type
+		// name and if so, return that schema name as the target object type
+		fmt.Printf("Looking up %s\n", guessObjectTypeName)
+		_, found := h.api.Components.Schemas[guessObjectTypeName]
+		if found {
+			return "*" + guessObjectTypeName
+		}
+		// TODO(jaypipes): Look through all schemas and try to match on known
+		// properties?
+	}
+	return ""
 }
 
 // getSchemaFromSchemaRef returns an openapi3.Schema given a SchemaRef
@@ -91,7 +166,6 @@ func (h *Helper) getSchemaFromSchemaRef(
 			schema, found := h.api.Components.Schemas[refSchemaID]
 			if found {
 				return schema.Value
-
 			}
 		}
 	} else {
