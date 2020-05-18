df845f1b0e3c8a358b7b9b11f4efb381a8dcbef4
Jay Pipes <jaypipes@gmail.com>
2020-05-18 13:14:49 -0400

    add example service pkg/resource package
    adds the pkg/resource package to the example service controller. There
    is a single bookResourceManager[Factory] that implements the
    types/AWSResourceManager[Factory] interface and glues the faked
    aws-sdk-go BookstoreAPI bindings that were introduced in the previous
    patch into the ACK runtime.

---
+++ services/example/pkg/resource/registry_test.go
@@ -0,0 +1,39 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//     http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package resource_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/aws/aws-service-operator-k8s/services/example/pkg/resource"
+)
+
+func TestRegistry(t *testing.T) {
+	require := require.New(t)
+
+	rmfs := resource.GetManagerFactories()
+	require.NotEmpty(rmfs)
+
+	// There should be a resource manager factory for Book resources
+	foundBookRMF := false
+	for _, rmf := range rmfs {
+		if rmf.GroupKind() == "example.services.k8s.aws:Book" {
+			foundBookRMF = true
+		}
+	}
+
+	require.True(foundBookRMF)
+}
