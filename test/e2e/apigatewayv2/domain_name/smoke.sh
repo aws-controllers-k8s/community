3a3a504137fdc1874a8d54c645dcb21156cc0286
vijtrip2 <62766875+vijtrip2@users.noreply.github.com>
2020-09-11 07:23:58 -0700

    apigatewayv2 service controller and smoke test files. (#293)
    * apigatewayv2 service controller and smoke test files.

---
+++ test/e2e/apigatewayv2/domain_name/smoke.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ROOT_DIR="$THIS_DIR/../../../.."
+SCRIPTS_DIR="$ROOT_DIR/scripts"
+
+source "$SCRIPTS_DIR/lib/common.sh"
+source "$SCRIPTS_DIR/lib/k8s.sh"
+source "$SCRIPTS_DIR/lib/testutil.sh"
+
+check_is_installed jq
+
+test_name="$( filenoext "${BASH_SOURCE[0]}" )"
+service_name="apigatewayv2"
+ack_ctrl_pod_id=$( controller_pod_id )
+debug_msg "executing test: $service_name/$test_name"
+
+echo "TODO: vijat@ domain_name smoke test"
+
+assert_pod_not_restarted $ack_ctrl_pod_id
