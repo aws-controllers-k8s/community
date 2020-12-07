352ba9c9f295b8d215c26b61d06ab79a3ad647ff
Nicolas Nosenzo <niconosenzo@gmail.com>
2020-12-07 14:24:47 +0100

    removed getopts strategy from publish-controller-image.sh (#554)
    * removed getopts strategy from publish-controller-image.sh

--- scripts/publish-controller-image.sh
+++ scripts/publish-controller-image.sh
@@ -6,7 +6,6 @@ DIR=$(cd "$(dirname "$0")"; pwd)
 SCRIPTS_DIR=$DIR
 DEFAULT_DOCKER_REPOSITORY="amazon/aws-controllers-k8s"
 DOCKER_REPOSITORY=${DOCKER_REPOSITORY:-$DEFAULT_DOCKER_REPOSITORY}
-OPTIND=1
 VERSION=$(git describe --tags --always --dirty || echo "unknown")
 
 source $SCRIPTS_DIR/lib/common.sh
@@ -15,44 +14,32 @@ check_is_installed docker
 
 USAGE="
 Usage:
-  $(basename "$0") -s <AWS_SERVICE> -r <DOCKER_REPOSITORY> [-i <Docker image tag>]
+  $(basename "$0") <AWS_SERVICE> 
 
 Publishes the Docker image for an ACK service controller. By default, the
 repository will be $DEFAULT_DOCKER_REPOSITORY and the image tag for the
 specific ACK service controller will be ":\$SERVICE-\$VERSION".
 
-Example: $(basename "$0") -s ecr -r amazon/aws-controllers-k8s
+<AWS_SERVICE> AWS Service name (ecr, sns, sqs)
 
-Options:
-  -s          Provide AWS Service name (ecr, sns, sqs)
-  -i          Controller container image tag (default: ack-<service>-controller:$VERSION)
-  -r          Name for the Docker repository to push to (default: $DEFAULT_DOCKER_REPOSITORY)
+Example: 
+export DOCKER_REPOSITORY=aws-controllers-k8s
+$(basename "$0") ecr 
+
+Environment variables:
+  DOCKER_REPOSITORY:        Name for the Docker repository to push to 
+                            Default: $DEFAULT_DOCKER_REPOSITORY
+  AWS_SERVICE_DOCKER_IMG:   Controller container image tag 
+                            Default: aws-controllers-k8s:$AWS_SERVICE-$VERSION
 "
 
-# Process our input arguments
-while getopts "r:s:i:" opt; do
-  case ${opt} in
-    r ) # Docker repository name
-        DOCKER_REPOSITORY="${OPTARG}"
-      ;;
-    s ) # AWS Service name
-        AWS_SERVICE=$(echo "${OPTARG}" | tr '[:upper:]' '[:lower:]')
-      ;;
-    i ) # Controller image tag
-        AWS_SERVICE_DOCKER_IMG="${OPTARG}"
-      ;;
-    \? )
-        echo "${USAGE}" 1>&2
-        exit
-      ;;
-  esac
-done
-
-if [ -z "$AWS_SERVICE" ]; then
-  echo "AWS_SERVICE is not defined. Use flag -s <AWS_SERVICE> to build a container image of that service"
-  echo "(Example: $(basename "$0") -q -s ecr)"
-  exit  1
+if [ $# -ne 1 ]; then
+    echo "AWS_SERVICE is not defined. Script accepts one parameter, <AWS_SERVICE> to build that docker images of that service" 1>&2
+    echo "${USAGE}"
+    exit 1
 fi
+
+AWS_SERVICE=$(echo "$1" | tr '[:upper:]' '[:lower:]')
 
 DEFAULT_AWS_SERVICE_DOCKER_IMG_TAG="${AWS_SERVICE}-${VERSION}"
 : "${AWS_SERVICE_DOCKER_IMG_TAG:="$DOCKER_REPOSITORY:$DEFAULT_AWS_SERVICE_DOCKER_IMG_TAG"}"
