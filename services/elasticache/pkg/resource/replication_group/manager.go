5bd95d1ad15211e0035f1d3e685585809f7be57d
Raghav Muddur <raghavnm@amazon.com>
2020-12-10 15:05:20 -0800

    Update terminal conditions for SDK Find
    InvalidParameterCombination exception can be
    thrown for snapshot find.

--- services/elasticache/pkg/resource/replication_group/manager.go
+++ services/elasticache/pkg/resource/replication_group/manager.go
@@ -85,7 +85,7 @@ func (rm *resourceManager) ReadOne(
 	}
 	observed, err := rm.sdkFind(ctx, r)
 	if err != nil {
-		return nil, err
+		return rm.onError(r, err)
 	}
 	return rm.onSuccess(observed)
 }
