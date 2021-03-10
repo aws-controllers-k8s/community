172049cbea41140a540c77698e6d20da25946dfb
Kartik Kalamadi <kalamadi@amazon.com>
2021-03-10 15:50:05 -0800

    minor changes

--- test/e2e/sagemaker/tests/test_hpo.py
+++ test/e2e/sagemaker/tests/test_hpo.py
@@ -10,7 +10,7 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
-"""Integration tests for the SageMaker TrainingJob API.
+"""Integration tests for the SageMaker HyperParameterTuning API.
 """
 
 import boto3
