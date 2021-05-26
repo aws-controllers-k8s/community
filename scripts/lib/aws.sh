a519c50981a3b9dca22e0b38446a07fef777aaf8
Suraj Kota <surakota@amazon.com>
2021-05-26 05:12:52 +0000

    increase role session duration

--- scripts/lib/aws.sh
+++ scripts/lib/aws.sh
@@ -50,7 +50,7 @@ aws_generate_temp_creds() {
     JSON=$(daws sts assume-role \
            --role-arn "$ACK_ROLE_ARN"  \
            --role-session-name tmp-role-"$__uuid" \
-           --duration-seconds 3600 \
+           --duration-seconds 7200 \
            --output json || exit 1)
 
     AWS_ACCESS_KEY_ID=$(echo "${JSON}" | jq --raw-output ".Credentials[\"AccessKeyId\"]")
