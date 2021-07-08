542e74d23dc4db60a516e454b8316071b478339b
Nicholas Thomson <nithomso@amazon.com>
2021-07-08 14:35:33 -0700

    Delete tests and build scripts

--- scripts/lib/aws/kms.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-ROOT_DIR="$THIS_DIR/../../.."
-SCRIPTS_DIR="$ROOT_DIR/scripts"
-
-. $SCRIPTS_DIR/lib/common.sh
-. $SCRIPTS_DIR/lib/aws.sh
