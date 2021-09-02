635f276472fe1411d50b6252d27c99e7c65ebcbe
Nicholas Thomson <nithomso@amazon.com>
2021-09-02 16:15:39 -0700

    Changed font to Open Sans

--- docs/assets/js/mermaid.js
+++ docs/assets/js/mermaid.js
@@ -2,7 +2,7 @@ import mermaid from 'mermaid/dist/mermaid';
 
 var config = {
   theme: 'default',
-  fontFamily: '"Jost", -apple-system, blinkmacsystemfont, "Segoe UI", roboto, "Helvetica Neue", arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";',
+  fontFamily: '"Open Sans", -apple-system, blinkmacsystemfont, "Segoe UI", roboto, "Helvetica Neue", arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";',
 };
 
 document.addEventListener('DOMContentLoaded', () => {
