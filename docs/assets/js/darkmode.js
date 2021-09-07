e3457a92599ebd0205a052d0d7fef949f4a05397
Nicholas Thomson <RedbackThomson@users.noreply.github.com>
2021-09-07 12:23:16 -0700

    Fix documentation website missing styles (#934)
    Description of changes:
    Includes a number of fixes to the documentation website, mostly regarding missing styles. These styles were purged from the end CSS by PostCSS, because they are not explicitly being used in the layouts (but are used when generating the markdown as HTML).
    - Allowlist reference tables, hint boxes and code syntax highlighting in PostCSS
    - Move dark mode logic to header script for faster loading (still stutters)
    - Fix dark mode default dark/light theme logic
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- docs/assets/js/darkmode.js
+++ docs/assets/js/darkmode.js
@@ -1,9 +1,12 @@
-document.getElementById('mode').addEventListener('click', () => {
-  document.body.classList.toggle('dark');
-  localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
-});
+window.addEventListener('DOMContentLoaded', (event) => {
+  document.getElementById('mode').addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
+  });
 
-let systemPreferenceDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-if (localStorage.getItem('theme') === 'light' || !systemPreferenceDark) {
-  document.body.classList.remove('dark');
-}
+  let systemPreferenceDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  let savedPreference = localStorage.getItem('theme');
+  if (savedPreference === 'light' || (savedPreference === null && !systemPreferenceDark)) {
+    document.body.classList.remove('dark');
+  }
+});
