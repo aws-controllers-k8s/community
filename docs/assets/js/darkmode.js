1e69c33b84fdb225bc03b920f3962451d5d3a07c
Nicholas Thomson <nithomso@amazon.com>
2021-09-07 11:51:38 -0700

    Move dark mode to header script

--- docs/assets/js/darkmode.js
+++ docs/assets/js/darkmode.js
@@ -1,9 +1,11 @@
-document.getElementById('mode').addEventListener('click', () => {
-  document.body.classList.toggle('dark');
-  localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
-});
+window.addEventListener('DOMContentLoaded', (event) => {
+  document.getElementById('mode').addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
+  });
 
-let systemPreferenceDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-if (localStorage.getItem('theme') === 'light' || !systemPreferenceDark) {
-  document.body.classList.remove('dark');
-}
+  let systemPreferenceDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  if (localStorage.getItem('theme') === 'light' || !systemPreferenceDark) {
+    document.body.classList.remove('dark');
+  }
+});
